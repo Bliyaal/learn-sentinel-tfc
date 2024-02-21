beba272e7e7e3f1e2d031debcce481c39f7434c6
Michel Sylvestre - SYLM <sylm@premiertech.com>
2024-02-21 16:08:43 -0500

    change to premium

--- main.tf
+++ main.tf
@@ -18,5 +18,5 @@ resource "azurerm_servicebus_namespace" "example" {
   name                = "sylm-tfcloud"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  sku                 = "Standard"
+  sku                 = "Premium"
 }
